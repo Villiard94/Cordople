4f557e7183846c5acbe07d777f09d9021ea7b10d
Villiard <sim_villiard@hotmail.com>
2016-09-10 16:10:03 -0400

    Added packages

---
+++ Cordople/gulpfile.js
@@ -0,0 +1,49 @@
+﻿var gulp = require("gulp");
+
+gulp.task('tslint', function () {
+
+    // Built-in rules are at
+    // https://github.com/palantir/tslint#supported-rules
+    var tslintConfig = {
+        "rules": {
+            "semicolon": true,
+            "requireReturnType": true,
+            "requireParameterType": true,
+            "jsdoc-format": true,
+            "quotemark": [true, "single"],
+            "variable-name": [true,"allow-leading-underscore"]
+        }
+    };
+
+    return gulp.src(['scripts/**/*.ts', '!scripts/typings/**'])
+    //Custom rules can be added to configuration. rulesDirectory: 'folder/folder'
+    .pipe(tslint({ configuration: tslintConfig }))
+    .pipe(tslint.report('verbose', { emitError: true, reportLimit: 0 }));
+});
+
+require('es6-promise').polyfill();
+
+gulp.task('build.css.sass', function () {
+    gulp.src('./docs/md/scripts/components/materials/components/**/*.scss')
+    // Guilp-Sass runs the pre processor on the .scss files using Sass. 
+    // Gulp-AutoPrefixer post processes the .css files using PostCSS. 
+    // CSS and Folder structure is saved to destination folder. 
+    .pipe(sass().on('error', sass.logError)).pipe(prefixer({ browsers: ['last 2 versions'], cascade: false })).pipe(gulp.dest('./docs/md/www/css'));
+});
+
+gulp.task('install.tsd', function (callback) {
+    ts({
+        command: 'reinstall',
+        config: './docs/md/tsd.json'
+    }, callback);
+});
+
+var browserSync = require('browser-sync');
+
+gulp.task('browser.sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./docs/md/www/"
+        }
+    });
+});
---
+++ Cordople/scripts/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/createjs-lib/index.d.ts" />
+/// <reference path="globals/preloadjs/index.d.ts" />
+/// <reference path="globals/soundjs/index.d.ts" />
+/// <reference path="globals/systemjs/index.d.ts" />
